1dd043f38346cf3b082c084c4344cc31fc2d1057
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-10-10 15:15:34 +0000

    ログイン系、トップ画面の作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.active_job.queue_adapter = :inline
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -73,4 +74,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts.clear
 end
