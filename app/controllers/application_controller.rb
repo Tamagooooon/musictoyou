9e7b7fbe2069c6f28bf25f4e345e0a314b217522
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-10-18 14:55:12 +0000

    AWS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action do # ActiveStorageのurlメソッドを使用できるようにする
+    ActiveStorage::Current.host = request.base_url
+  end
+
+
   def after_sign_in_path_for(resource)
     posts_path
   end
--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -25,7 +25,7 @@ class User::PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_path
+    redirect_to posts_path 
   end
 
   private
