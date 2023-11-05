9ed6248a01ec2ead3396f683c7dd9cccc7daf162
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-1.compute.internal>
2023-11-05 13:55:44 +0000

    レイアウト調整

--- Gemfile
+++ Gemfile
@@ -64,6 +64,8 @@ group :production do
   gem 'mysql2'
 end
 
+gem 'jquery-rails'
+
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -3,13 +3,13 @@ class User::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to posts_path
+    #redirect_to posts_path
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to posts_path
+    #redirect_to posts_path
   end
 end
